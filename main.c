76149a84a5251d8ea4bc4030f975289b72839f20
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-11-07 21:41:33 -0600

    Creacion de carpeta libreria

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include "librerias/data_of_users.h"
 
-main(){
+int main(){
+    int i;
+    for (i = 0; i < nips[i]; i++) {
+        printf("\nNip %d: %d", i, nips[i]);
+    }
 
     getch();
     return 0;

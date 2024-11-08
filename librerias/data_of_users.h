277d2f6420da52381d0d7f4e64d0a44203988343
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-11-07 22:40:57 -0600

    Definicion de la estructura usuarios y pequeñas pruebas

--- librerias/data_of_users.h
+++ librerias/data_of_users.h
@@ -1,6 +1,14 @@
 #ifndef DATA_OF_USERS_H_INCLUDED
 #define DATA_OF_USERS_H_INCLUDED
 
-extern int nips[10] = {1, 2, 4, 5, 90, 23};
+struct Users {
+    char name[50];
+    char lastname[50];
+    long long int phone_number;
+    long long int number_card;
+    long long int nip;
+};
+
+extern struct Users data_users[];
 
 #endif // DATA_OF_USERS_H_INCLUDED
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>
 #include "librerias/data_of_users.h"
 
 void menu_principal();
@@ -12,10 +13,29 @@ void transfer_money();
 void recharge_airtime();
 void change_nip();
 
+struct Users data_users[];
+
 int main(){
+    strcpy(data_users[0].name, "Jorge");
+    strcpy(data_users[0].lastname, "Ozuna");
+    data_users[0].nip = 3434;
+    data_users[0].number_card = 34342309324;
+    data_users[0].phone_number = 3315660281;
+
+    strcpy(data_users[1].name, "Emma");
+    strcpy(data_users[1].lastname, "Myers");
+    data_users[1].nip = 7777;
+    data_users[1].number_card = 99999999999;
+    data_users[1].phone_number = 903298493984;
+
     int i;
-    for (i = 0; i < nips[i]; i++) {
-        printf("\nNip %d: %d", i, nips[i]);
+    for(i = 0; i < 2; i++) {
+        printf("\n--------------------------");
+        printf("\nNombre: %s", data_users[i].name);
+        printf("\nApellido: %s", data_users[i].lastname);
+        printf("\nNip: %d", data_users[i].nip);
+        printf("\nNumber card: %lld", data_users[i].number_card);
+        printf("\nNumber phone: %lld", data_users[i].phone_number);
     }
 
     getch();

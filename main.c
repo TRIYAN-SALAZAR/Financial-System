12b11c660b05ccb10be778b9a6202ca2575ffb9e
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-11-07 21:57:51 -0600

    Declaracion de funciones

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 #include <windows.h>
 #include "librerias/data_of_users.h"
 
+void menu_principal();
+void add_new_user();
+void deposit_own_acc();
+void check_card();
+void transfer_money();
+void recharge_airtime();
+void change_nip();
+
 int main(){
     int i;
     for (i = 0; i < nips[i]; i++) {
@@ -13,3 +21,4 @@ int main(){
     getch();
     return 0;
 }
+

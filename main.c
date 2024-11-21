cfc29aa75bd95f37ada1baa03eb61d8b9df50e42
Marioalejn <152515406+Marioalejn@users.noreply.github.com>
2024-11-21 13:39:39 -0600

    Update main.c --> recharge_airtime and add & to all the scanf

--- main.c
+++ main.c
@@ -114,10 +114,11 @@ void add_new_user() {
     scanf("%4s", u1.nip);
 
     u1.saldo=0;
-    data_users[cont_users] = u1; //Guardar los datos ingresados en el arreglo del struct
+    data_users[cont_users] = u1; //Guardar los datos ingresados en el arreglo del struct 
     cont_users++;
     printf("Tu registro fue exitoso, regresando al menu...");
     printf("%c", data_users[cont_users].nip);
+
 }
 
 void deposit_own_acc() {}
@@ -125,15 +126,15 @@ void deposit_own_acc() {}
 void check_card() {}
 
 void transfer_money() {
-//encontrar si el usuario existe con su numero y nip
+   //encontrar si el usuario existe con su numero y nip
     char verification_pnum[10], verification_nip[4], verification_card[16];
     int i, validador=0, transferencia, verification_transfer, cliente;
     printf("\nIngresa tu numero de telefono: ");
-    scanf("%10s", verification_pnum);
+    scanf("%10s", &verification_pnum);
     for(i=0;i<cont_users;i++){
         if (strcmp(verification_pnum, data_users[i].phone_number) == 0) {
             printf("\nIngresa tu NIP: ");
-            scanf("%4s", verification_nip);
+            scanf("%4s", &verification_nip);
             if (strcmp(verification_nip, data_users[i].nip) == 0){
                 printf("%s ", data_users[i].name);
                 printf("%s \n", data_users[i].lastname);
@@ -141,22 +142,22 @@ void transfer_money() {
                 cliente = i; //para si se llega a concluir, saber a quien quitarle dinero
                 validador = 1; //si sus datos estan bien, seguimos con la siguiente parte
             }
-
-        }
+        
+        } 
     }
     if(validador == 0){
         printf("Los datos ingresados son erroneos");
     }
-
-    if(validador == 1){//inicio del if si puso bien sus datos
+    
+    if(validador == 1){//inicio del if si puso bien sus datos 
         printf("Ingresa cuanto quieres transferir: ");
         scanf("%i", &transferencia);
         if( data_users[cliente].saldo > transferencia){
             printf("Ingresa su numero de tarjeta: ");
-            scanf("%s", verification_card);
+            scanf("%s", &verification_card);
             for(i=0;i<cont_users;i++){
                 if (strcmp(verification_card, data_users[i].number_card) == 0) {
-                    printf("El numero de cuenta %s pertenece a %s %s",verification_card, data_users[i].name, data_users[i].lastname);
+                    printf("El numero de cuenta %s pertenece a %s %s",verification_card, data_users[i].name, data_users[i].lastname); 
                     printf("\nEstas seguro de que quieres transferirle %i pesos? \n1.Si 2.No: ", transferencia);
                     scanf("%i", &verification_transfer);
                     if(verification_transfer == 1){
@@ -180,7 +181,64 @@ void transfer_money() {
 
 }
 
-void recharge_airtime() {}
+void recharge_airtime() {
+        char verification_pnum[10], verification_nip[4], verification_card[16], pnum_to_transfer[0];
+    int i, new_try, validador=0, cliente, compañia, cantidad_transferencia;
+    printf("\nIngresa tu numero de telefono: ");
+    scanf("%10s", &verification_pnum);
+    for(i=0;i<cont_users;i++){
+        if (strcmp(verification_pnum, data_users[i].phone_number) == 0) {
+            printf("\nIngresa tu NIP: ");
+            scanf("%4s", &verification_nip);
+            if (strcmp(verification_nip, data_users[i].nip) == 0){
+                    printf("%s ", data_users[i].name);
+                    printf("%s \n", data_users[i].lastname);
+                    printf("saldo: %i\n", data_users[i].saldo);
+                    cliente = i; //para si se llega a concluir, saber a quien quitarle dinero
+                    validador = 1; //si sus datos estan bien, seguimos con la siguiente parte
+                    printf("A qué compañía quiere hacer la recarga de tiempo aire?\n 1. AT%cT 2. Telcel 3. Movistar: ", 38);
+                    scanf("%i", &compañia);
+                    printf("Ingresa la cantidad que quieres recargar: ");
+                    scanf("%i", &cantidad_transferencia);
+                    printf("A que numero quieres transferirlo?: ");
+                    scanf("%s", &pnum_to_transfer[0]);
+                        if(data_users[cliente].saldo < cantidad_transferencia){
+                            printf("No tienes los fondos suficientes para hacer la transferencia, vuelve a registrar tus datos...");
+                            recharge_airtime();
+                        }
+                        if(data_users[cliente].saldo > cantidad_transferencia){
+                            printf("Todo esta bien?, quieres concluir la transferencia?\n 1.Si 2.No: ");
+                            scanf("%i", &validador);
+                            if(validador == 1){
+                                data_users[i].saldo += cantidad_transferencia;
+                                data_users[cliente].saldo -= cantidad_transferencia;
+                                printf("Recarga exitosa\n La recarga se hizo al numero: %s\n El monto de la recarga es: %i\n ", pnum_to_transfer[0], cantidad_transferencia);
+                                menu_principal();
+                            }
+                            if(validador == 2){
+                                system("cls");
+                                printf("Vuelve a ingresar tus datos");
+                                menu_principal();
+                            }
+                        }
+            }   
+        }
+    }
+
+    
+    //cuando sus datos ingresados al inicio son erroneos
+    if(validador == 0){
+        printf("Los datos ingresados son erroneos... \n ¿Quieres intentarlo de nuevo? 1.Si 2.No: ");
+        scanf("%i", &new_try);
+        if(new_try == 1){
+            recharge_airtime();
+        }
+        else{
+            printf("Regresando al menu...");
+            menu_principal();
+        }
+    }
+}
 
 void change_nip() {}
 
